a8da496f901818839d93d2206ec7acd48d573053
Rahul <rahul.samaranayake@zendesk.com>
2022-12-03 16:17:20 +1100

    frontend typescript updates

---
+++ typescript/react-crud-video/frontend/src/features/posts/postSlice.tsx
@@ -0,0 +1,15 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import produce from "immer";
+import { RootState } from "../../app/store";
+
+const initialState: any = {
+    posts: [
+        {
+            id: 0,
+            title: "",
+            body: "",
+            created_at: "",
+            updated_at: "",
+        }
+    ]
+}

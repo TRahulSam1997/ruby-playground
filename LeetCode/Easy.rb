dc081ba7e4372d7e1cd2ef941f9027b682fd5602
Rahul <rahul.samaranayake@zendesk.com>
2023-02-11 16:03:50 +1300

    145 Binary Tree Postorder Traversal solution

---
+++ LeetCode/Easy.rb
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode
+    attr_accessor :val, :left, :right
+    def initialize(val = 0, left = nil, right = nil)
+        @val = val
+        @left = left
+        @right = right
+    end
+end
+# @param {TreeNode} root
+# @return {Integer[]}
+def postorder_traversal(root)
+  return [] if root.nil?
+  left_t = postorder_traversal(root.left)
+  right_t = postorder_traversal(root.right)
+  left_t + right_t + [root.val]
+end

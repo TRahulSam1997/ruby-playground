e97a3758e15b4db0248aca2c83456c1622d23600
Rahul <rahul.samaranayake@zendesk.com>
2023-02-18 10:25:41 +1300

    Ruby Intersection of Two Linked Lists solution

--- LeetCode/Easy.rb
+++ LeetCode/Easy.rb
@@ -1,3 +1,53 @@
+# Definition for singly-linked list.
+class ListNode
+  attr_accessor :val, :next
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
+
+# @param {ListNode} headA
+# @param {ListNode} headB
+# @return {ListNode}
+def getIntersectionNode(headA, headB)
+  len1 = get_length(headA)
+  len2 = get_length(headB)
+
+  if len1 > len2
+    headA = move_head_by_distance(headA, len1 - len2)
+  elsif len2 > len1
+    headB = move_head_by_distance(headB, len2 - len1)
+  end
+
+  while !headA.nil? && !headB.nil?
+    return headA if headA == headB
+
+    headA = headA.next
+    headB = headB.next
+  end
+
+  nil
+end
+
+def get_length(head)
+  length = 0
+  current = head
+  until current.nil?
+    length += 1
+    current = current.next
+  end
+  length
+end
+
+def move_head_by_distance(head, distance)
+  current = head
+  distance.times do
+    current = current.next
+  end
+  current
+end
+
 # Definition for a binary tree node.
 class TreeNode
     attr_accessor :val, :left, :right
